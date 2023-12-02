a1af7a13f1f1b0b20fdf822cc37b7fb0f87c5292
Sengkue <skvgithut@gmail.com>
2023-12-02 12:05:35 +0700

    update css

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,6 +1,8 @@
 // src/components/Translation.tsx
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Translation.css'; // Import the CSS file
+import { FaTimesCircle } from "react-icons/fa";
 
 const Translation: React.FC = () => {
   const [text, setText] = useState('');
@@ -11,7 +13,7 @@ const Translation: React.FC = () => {
       const response = await axios.get(
         `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
           text
-        )}&langpair=en|th`,
+        )}&langpair=hmn|en`
       );
 
       // Extract translated text from the response
@@ -22,11 +24,28 @@ const Translation: React.FC = () => {
     }
   };
 
+  const handleClear = () => {
+    setText('');
+    setTranslatedText('');
+  };
+
   return (
-    <div>
-      <textarea value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleTranslate}>Translate</button>
-      <div>
+    <div className="translation-container">
+      <textarea
+        className="input-text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter text to translate..."
+      />
+      <div className="button-container">
+        <button className="translate-button" onClick={handleTranslate}>
+          Translate
+        </button>
+        <button className="clear-button" onClick={handleClear}>
+          <FaTimesCircle /> {/* Use the close icon here */}
+        </button>
+      </div>
+      <div className="result-container">
         <strong>Translated Text:</strong> {translatedText}
       </div>
     </div>
